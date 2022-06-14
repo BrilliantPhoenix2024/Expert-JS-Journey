29aaf8edd38fb013900cabb7bf581d27f268858d
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-06-14 22:36:20 +0330

    Values and Variables

---
+++ Fundamental-Part1/Value&variable/script.js
@@ -0,0 +1,26 @@
+// write Variable in Camelcase StyleSheet.
+// Don't Write Variable in reserved JS Name.
+// Don't Write Variable in upercase letter.
+
+const PI = 3.1415;
+console.log(PI)
+
+// Write clean code.
+let bigDog = "Asal";
+let babyDog = "Rony";
+
+// Don't name Variable like below.
+let dog1 = "Asal";
+let dog2 = "Rony"
+
+let firstName = "Atefeh";
+console.log(firstName)
+
+let num = 23;
+console.log(num)
+
+let language = "JavaScript"
+console.log(language)
+
+console.log("phoenix")
+console.log(2024)
