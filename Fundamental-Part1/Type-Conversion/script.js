36e2c5b83128b491808dcc24f592cb16e9e1bec5
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-06-15 17:53:50 +0330

    Type Conversion and Coercion

---
+++ Fundamental-Part1/Type-Conversion/script.js
@@ -0,0 +1,28 @@
+// Type Conversion
+
+const inputYear = Number('1990');
+console.log(inputYear + 18);
+
+console.log(Number('Atefeh'));
+console.log(typeof NaN);
+
+console.log(String(23));
+
+// Type Coercion
+
+console.log('I am ' + 32 + ' years old');
+console.log('I am ' + '32' + ' years old');
+
+console.log('23' - '10' - 3);
+console.log('23' + '10' + 3);
+console.log('23' * '2');
+console.log('23' / '2');
+
+console.log('23' > '8');
+
+let n = '1' + 1;
+n = n - 1;
+console.log(n);
+
+console.log(2 + 3 + 4 + '5');
+console.log('10' - '4' - '3' - 2 + '5');
