3e8a0c41add22dfa59ee9eb73cb804dc593db1c3
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-06-16 18:08:43 +0330

    Statments and Expressions

---
+++ Fundamental-Part1/Statements-Expressions/script.js
@@ -0,0 +1,14 @@
+4 + 4 // an Expression --> produce Value.
+2022  // an Expression --> produce Value.
+true && false && !false // both an Expression and a statement.
+
+
+/* a Statements --> 
+doesn't produce Value and translate actions that we want to programme to perform
+*/
+
+if (18 < 32) {
+    const str = '32 is bigger';
+}
+
+// in Template literal we must only use Expressions NOT Statements.
