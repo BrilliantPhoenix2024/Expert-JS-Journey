3aa88691a814727ee9f540e163b96ab77c8bfc98
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-06-19 00:06:59 +0330

    introduction to Objects

---
+++ Fundamental-Part2/Objects/script.js
@@ -0,0 +1,16 @@
+const Atefeh = {
+    firstName : 'Atefeh',
+    lastName : 'Mohammadi',
+    job : 'Student',
+    age : 2022 - 1990,
+    myStatus : {
+        currentLearning : ['JaveScript', 'ReactJS'],
+        currentWorking : ['Python', 'Django'],
+    }, 
+    friends : ['Sara', 'Amir', 'Shayan', 'Mohammad'],
+}
+
+console.log(Atefeh.myStatus.currentLearning[0]);
+console.log(Atefeh.myStatus.currentWorking[1]);
+console.log(Atefeh);
+console.log(Atefeh.friends[3]);
