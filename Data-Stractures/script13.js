4d9acfd8270c89078020cdcf2259a899b32582a1
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-06-30 22:48:42 +0330

    Maps Iteration completed

--- Data-Stractures/script13.js
+++ Data-Stractures/script13.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const weekDays = ['mon', 'tue', 'wed', 'thr', 'fri', 'sat', 'sun'];
-const opennightHours = {
+const openingHours = {
   [weekDays[3]]: {
     open: 12,
     close: 22,
@@ -22,7 +22,7 @@ const restaurant = {
   categories: ["Organic", "Traditional", "Iranian"],
   starterMenu: ["Salad", "Bread", "Soupe", "Aash"],
   mainMenu: ["Kebab", "Satori", "Jojeh"],
-  opennightHours,
+  openingHours,
 
   order (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
@@ -50,3 +50,39 @@ const restaurant = {
     console.log(otherIngredients);
   },
 };
+
+const question = new Map([
+  ['question', 'Q1'],
+  [1, 'Ans1'],
+  [2, 'Ans2'],
+  [3, 'Ans3'],
+  ['correct', 3],
+  [true,'Correct'],
+  [false, 'Try again!'],
+]);
+console.log(question);
+
+// Convert Object to Map
+console.log(Object.entries(openingHours));
+const hourMap = new Map(Object.entries(openingHours));
+console.log(hourMap);
+
+// Map Iteration
+// Quiz app
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`)
+};
+
+// const answer = Number(prompt('Your answer: '));
+const answer = 3;
+console.log(answer);
+console.log(
+  question.get(question.get('correct') === answer)
+);
+
+// Convert Map to Array
+console.log([...question]);
+// console.log(question.entries());
+console.log([...question.keys()]);
+console.log([...question.values()]);
