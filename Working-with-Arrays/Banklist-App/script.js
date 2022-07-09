3dd6f5312f3c19d660e3268f980e611da94bb57a
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-07-10 01:31:36 +0330

    Modify 'calcdisplaySummery' and 'calcDisplayBalance' Function

--- Working-with-Arrays/Banklist-App/script.js
+++ Working-with-Arrays/Banklist-App/script.js
@@ -82,26 +82,26 @@ const displayMovements = function (movements) {
 displayMovements(account1.movements);
 
 // Computing Balance
-const calcDisplayBalance = function (movements) {
-  const balance = movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${balance} €`;
+const calcDisplayBalance = function (acc) {
+  acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${acc.balance} €`;
 };
 
 // Computing IN and OUT and INTEREST in Summery section
-const calcdisplaySummery = function (movements) {
-  const incomes = movements
+const calcdisplaySummery = function (acc) {
+  const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
   labelSumIn.textContent = `${incomes}€`;
 
-  const out = movements
+  const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
   labelSumOut.textContent = `${Math.abs(out)} €`;
 
-  const interest = movements
+  const interest = acc.movements
     .filter(mov => mov > 0)
-    .map(deposit => (deposit * 1.2) / 100)
+    .map(deposit => (deposit * acc.interestRate) / 100)
     .filter((interest, i, arr) => {
       // console.log(arr);
       return interest >= 1;
@@ -148,13 +148,7 @@ btnLogin.addEventListener('click', function (e) {
     inputLoginUsername.value = inputLoginPin.value = '';
     inputLoginPin.blur();
 
-    // Display movements
-    displayMovements(currentAccount.movements);
-
-    // Display balance
-    calcDisplayBalance(currentAccount.movements);
-
-    // Display summary
-    calcdisplaySummery(currentAccount.movements);
-  };
+    // Update UI
+    updateUI(currentAccount);
+  }
 });
