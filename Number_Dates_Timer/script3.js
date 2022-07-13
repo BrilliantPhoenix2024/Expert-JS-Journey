fa21ea83b66ad3b1a89d97d1e0f2ca2434b69f44
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-07-13 20:38:33 +0330

    The Reminder Operator

---
+++ Number_Dates_Timer/script3.js
@@ -0,0 +1,21 @@
+// The Reminder Operator
+'use strict';
+
+console.log(5 % 2);
+console.log(5 / 2); // 5 = 2 * 2 + 1
+
+console.log(8 % 3);
+console.log(8 / 3); // 8 = 2 *3 + 2
+
+// Odd Number
+console.log(6 % 2);
+console.log(6 / 2);
+
+// Even Number
+console.log(7 % 2);
+console.log(7 / 2);
+
+const isEven = n => n % 2 === 0;
+console.log(isEven(8));
+console.log(isEven(32));
+console.log(isEven(2023));
