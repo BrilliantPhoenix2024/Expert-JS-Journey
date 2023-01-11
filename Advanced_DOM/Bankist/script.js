ac0a02f609d8f843fc7548c4c7e89345e13465e4
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-01-11 18:02:11 +0330

    Menu fade animation(Passing "arguments" into event handlers)

--- Advanced_DOM/Bankist/script.js
+++ Advanced_DOM/Bankist/script.js
@@ -4,6 +4,12 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+const nav = document.querySelector('.nav');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
 
 ///////////////////////////////////////
 // Modal window Function
@@ -35,9 +41,6 @@ document.addEventListener('keydown', function (e) {
 
 ////////////////////////////////
 // Implementing smoth Button Scrolling
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
-
 btnScrollTo.addEventListener('click', function (e) {
   const s1coords = section1.getBoundingClientRect();
   console.log(s1coords);
@@ -98,10 +101,6 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 
 /////////////////////////////////
 // Tabbed Component
-const tabs = document.querySelectorAll('.operations__tab');
-const tabsContainer = document.querySelector('.operations__tab-container');
-const tabsContent = document.querySelectorAll('.operations__content');
-
 tabsContainer.addEventListener('click', function (e) {
   const clicked = e.target.closest('.operations__tab');
 
@@ -120,3 +119,37 @@ tabsContainer.addEventListener('click', function (e) {
     .querySelector(`.operations__content--${clicked.dataset.tab} `)
     .classList.add('operations__content--active');
 });
+
+/////////////////////////////////
+
+// Menu fade animation
+const handelHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+// Solution 1:
+// nav.addEventListener('mouseover', function (e) {
+//   handelHover(e, 0.5);
+// });
+
+// Solution 2:
+// Passing "arguments" into event handlers
+nav.addEventListener('mouseover', handelHover.bind(0.5));
+
+// Solution 1:
+// nav.addEventListener('mouseout', function (e) {
+//   handelHover(e, 1);
+// });
+
+// Solution 2:
+// Passing "arguments" into event handlers
+nav.addEventListener('mouseout', handelHover.bind(1));
