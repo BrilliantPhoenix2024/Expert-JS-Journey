39adddead216d3bee94f44881ccc2058da99ae08
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-01-15 00:29:47 +0330

    Revealing Elements on Scroll (Reveal Sections)

--- Advanced_DOM/Bankist/script.js
+++ Advanced_DOM/Bankist/script.js
@@ -169,7 +169,6 @@ const navHight = nav.getBoundingClientRect().height;
 
 const stickyNav = function (entries) {
   const [entry] = entries;
-  console.log(entry);
   if (!entry.isIntersecting) nav.classList.add('sticky');
   else nav.classList.remove('sticky');
 };
@@ -179,3 +178,26 @@ const headerObserver = new IntersectionObserver(stickyNav, {
   rootMargin: `-${navHight}px`,
 });
 headerObserver.observe(header);
+
+/////////////////////////////////
+// Revealing Elements on Scroll
+// Reveal Sections
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
