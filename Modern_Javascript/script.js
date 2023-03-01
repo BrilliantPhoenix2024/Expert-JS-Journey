6f21c307294772a201114b25b2b2d2ec50a19b40
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-03-01 20:52:13 +0330

    Modern-JS _ The Module Patern

--- Modern_Javascript/script.js
+++ Modern_Javascript/script.js
@@ -18,5 +18,37 @@ import add, { cart } from './shoppingCart.js';
 add('pizza', 2);
 add('bread', 5);
 add('apple', 4);
-
 console.log(cart);
+
+///////////////////////////////////
+// The Module Patern
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 232;
+  const totalQuantity = 32;
+
+  const addToCart = function (product, quantity) {
+    cart.push({
+      product,
+      quantity,
+    });
+    console.log(`${quantity} ${product} was added to cart. `);
+  };
+
+  const orderStock = function (product, quantity) {
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+ShoppingCart2.addToCart('apple', 4);
+ShoppingCart2.addToCart('pizza', 2);
+console.log(ShoppingCart2);
+console.log(ShoppingCart2.shippingCost);
