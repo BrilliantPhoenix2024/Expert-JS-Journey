236a3c9399f4c2ad561ccd6a117732a696d20d33
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-03-11 16:10:08 +0330

    ForkifyApp_ Helpers and Configuration Files

--- ForkifyApp/src/js/model.js
+++ ForkifyApp/src/js/model.js
@@ -1,4 +1,6 @@
 import { async } from 'regenerator-runtime';
+import { API_URL } from './config.js';
+import { getJson } from './helpers.js';
 
 export const state = {
   recipe: {},
@@ -6,12 +8,8 @@ export const state = {
 
 export const loadRecipe = async function (id) {
   try {
-    const res = await fetch(
-      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
-    );
-    const data = await res.json();
+    const data = await getJson(`${API_URL}/${id}`);
 
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
     const { recipe } = data.data;
     state.recipe = {
       id: recipe.id,
@@ -25,6 +23,7 @@ export const loadRecipe = async function (id) {
     };
     console.log(state.recipe);
   } catch (err) {
-    alert(err);
+    // Temp error handling
+    console.error(`${err}!!!!`);
   }
 };
