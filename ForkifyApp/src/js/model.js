de984feac97211f0241c8ab14d2a93d015230553
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-03-15 00:32:34 +0330

    Implementing Search Results - Part 1

--- ForkifyApp/src/js/model.js
+++ ForkifyApp/src/js/model.js
@@ -4,11 +4,15 @@ import { getJson } from './helpers.js';
 
 export const state = {
   recipe: {},
+  search: {
+    query: '',
+    results: [],
+  },
 };
 
 export const loadRecipe = async function (id) {
   try {
-    const data = await getJson(`${API_URL}/${id}`);
+    const data = await getJson(`${API_URL}${id}`);
 
     const { recipe } = data.data;
     state.recipe = {
@@ -24,7 +28,30 @@ export const loadRecipe = async function (id) {
     console.log(state.recipe);
   } catch (err) {
     // Temp error handling
-    console.error();
+    console.error(`${err} !!!!`);
+    throw err;
+  }
+};
+
+export const loadSearchResults = async function (query) {
+  try {
+    state.search.query = query;
+
+    const data = await getJson(`${API_URL}?search=${query}`);
+    console.log(data);
+
+    state.search.results = data.data.recipes.map(rec => {
+      return {
+        id: rec.id,
+        title: rec.title,
+        publisher: rec.publisher,
+        image: rec.image_url,
+      };
+    });
+
+    
+  } catch (err) {
+    console.error(`${err} !!!!`);
     throw err;
   }
 };
