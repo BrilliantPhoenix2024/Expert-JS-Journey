5ddab49114b2e3d6a2a9ad4da240409e36c4e083
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-03-20 15:49:04 +0330

    Forkify App_ Uploading a New Recipe - Part 2

--- ForkifyApp/src/js/config.js
+++ ForkifyApp/src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = '991dce7b-afc8-4aa4-8b7c-68c053187c99';
+export const MODAL_CLOSE_SEC = 2.5;
