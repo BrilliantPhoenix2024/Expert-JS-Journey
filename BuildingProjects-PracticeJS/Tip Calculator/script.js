44e482296c2b021857a95806f9b4e86cb0a43848
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-03-22 22:44:05 +0330

    change the output Text

--- BuildingProjects-PracticeJS/Tip Calculator/script.js
+++ BuildingProjects-PracticeJS/Tip Calculator/script.js
@@ -6,6 +6,5 @@ console.log(button);
 button.addEventListener("click", function () {
   console.log(cost.value * 0.15);
   let tip = (cost.value * 0.15).toFixed(2);
-  let temp = `<p>tip $${tip} on $${cost.value}<p>`;
-  output.innerHTML = temp;
+  output.innerText = tip;
 });
