39fe856b0fa0cd4f0f27f9fdb2fa918b0d1818db
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-03-22 22:40:07 +0330

    Backtick template in JavaScript

--- BuildingProjects-PracticeJS/Tip Calculator/script.js
+++ BuildingProjects-PracticeJS/Tip Calculator/script.js
@@ -5,5 +5,7 @@ const cost = document.querySelector("input");
 console.log(button);
 button.addEventListener("click", function () {
   console.log(cost.value * 0.15);
-  output.innerText = "output";
+  let tip = (cost.value * 0.15).toFixed(2);
+  let temp = `<p>tip $${tip} on $${cost.value}<p>`;
+  output.innerHTML = temp;
 });
