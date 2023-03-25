8c15225022df07dc2752b28ba4ef6d995c534575
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-03-25 20:30:27 +0330

    Element Interaction JavaScript

---
+++ BuildingProjects-PracticeJS/PopUp Message/script.js
@@ -0,0 +1,10 @@
+const popups = document.querySelectorAll('.popup');
+
+for (let x = 0; x < popups.length; x++) {
+  console.log(popups[x]);
+  popups[x].addEventListener('click', function () {
+    // console.log(this);
+    let outputText = this.getAttribute('data-message');
+    console.log(outputText);
+  });
+}
