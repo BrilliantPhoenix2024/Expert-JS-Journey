2fea738fb33fdddb08c70cc9996900aa414371b4
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-03-25 20:52:03 +0330

    Hide Show PopUp

--- BuildingProjects-PracticeJS/PopUp Message/script.js
+++ BuildingProjects-PracticeJS/PopUp Message/script.js
@@ -1,10 +1,22 @@
 const popups = document.querySelectorAll('.popup');
+const popup = document.querySelector('.output');
+const popupMessage = document.querySelector('.message');
+const closeBtn = document.querySelector('.close');
+
+closeBtn.addEventListener('click', function () {
+  popup.classList.add('hide');
+});
 
 for (let x = 0; x < popups.length; x++) {
   console.log(popups[x]);
   popups[x].addEventListener('click', function () {
     // console.log(this);
     let outputText = this.getAttribute('data-message');
-    console.log(outputText);
+    showMessage(outputText);
   });
+}
+
+function showMessage(output) {
+  popup.classList.remove('hide');
+  popupMessage.innerText = output;
 }
