fc5585574ef893d9b79581adbb02f512743c4d9d
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-03-27 20:58:47 +0330

    Add Style in JavaScript

--- BuildingProjects-PracticeJS/Functionator/script.js
+++ BuildingProjects-PracticeJS/Functionator/script.js
@@ -3,5 +3,14 @@ document.addEventListener("DOMContentLoaded", function () {
   console.log("ready!");
   myBlock = document.createElement("div");
   myBlock.textContent = "Hello World";
+  myBlock.style.width = "100px";
+  myBlock.style.height = "100px";
+  myBlock.style.backgroundColor = "purple";
+  myBlock.style.color = "white";
+  myBlock.style.lineHeight = "100px";
+  myBlock.style.textAlign = "center";
+  myBlock.style.position = "absolute";
+  myBlock.style.top = "100px";
+  myBlock.style.left = "150px";
   document.body.appendChild(myBlock);
 });
