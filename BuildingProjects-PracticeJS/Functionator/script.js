e156841765ba82c48f490202dd28f3ff847a7079
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-03-27 20:57:27 +0330

    Setup Elements in JavaScript

--- BuildingProjects-PracticeJS/Functionator/script.js
+++ BuildingProjects-PracticeJS/Functionator/script.js
@@ -1,1 +1,7 @@
-
+let myBlock;
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("ready!");
+  myBlock = document.createElement("div");
+  myBlock.textContent = "Hello World";
+  document.body.appendChild(myBlock);
+});
