f9090603bf02d9fea45441d8272ec82d89da8c94
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-03-29 22:48:17 +0330

    Element Mover

--- BuildingProjects-PracticeJS/Functionator/script.js
+++ BuildingProjects-PracticeJS/Functionator/script.js
@@ -27,16 +27,44 @@ document.addEventListener("keydown", function (e) {
   else if (keyC === 39) addFun("right");
   else if (keyC === 40) addFun("down");
   else if (keyC === 67) myBlock.style.backgroundColor = randomColor();
+  else if (keyC === 13 || keyC === 32) {
+    mover();
+  }
   // console.log(e.keyCode);
 });
 
+function mover() {
+  if (funList.length > 0) {
+    let cur = myBlock.getBoundingClientRect();
+    let el = funList.shift();
+    let item = el.textContent.replace("+", "");
+    myFunctionList.removeChild(el);
+    myBlock.innerHTML = "Move:" + item;
+    if (item == "left") {
+      myBlock.style.left = cur.left - cur.width + "px";
+    }
+    if (item == "right") {
+      myBlock.style.left = cur.left + cur.width + "px";
+    }
+    if (item == "up") {
+      myBlock.style.top = cur.top - cur.height + "px";
+    }
+    if (item == "down") {
+      myBlock.style.top = cur.top + cur.height + "px";
+    }
+    setTimeout(mover, 200)
+    console.log(item);
+    // console.log(el);
+    // console.log(cur);
+  }
+}
+
 function addFun(value) {
-  funList.push(value);
   let span = document.createElement("span");
   span.textContent = "+" + value;
   span.style.padding = "10px";
   span.style.border = "1px solid #ddd";
-  myFunctionList.appendChild(span);
+
   span.addEventListener("mouseover", function () {
     this.style.backgroundColor = "red";
     this.style.color = "white";
@@ -45,6 +73,8 @@ function addFun(value) {
     this.style.backgroundColor = "white";
     this.style.color = "black";
   });
+  myFunctionList.appendChild(span);
+  funList.push(span);
 
   console.log(funList);
 }
