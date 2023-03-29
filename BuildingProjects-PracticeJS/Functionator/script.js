514dd2f78823fa30613425d971e90cc1d3355e94
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-03-29 21:20:51 +0330

    Action List Maker

--- BuildingProjects-PracticeJS/Functionator/script.js
+++ BuildingProjects-PracticeJS/Functionator/script.js
@@ -1,4 +1,6 @@
 let myBlock;
+let myFunctionList;
+let funList = [];
 document.addEventListener("DOMContentLoaded", function () {
   console.log("ready!");
   myBlock = document.createElement("div");
@@ -13,18 +15,39 @@ document.addEventListener("DOMContentLoaded", function () {
   myBlock.style.top = "100px";
   myBlock.style.left = "150px";
   document.body.appendChild(myBlock);
+  myFunctionList = document.createElement("div");
+  document.body.appendChild(myFunctionList);
 });
 
 document.addEventListener("keydown", function (e) {
   e.preventDefault();
   let keyC = e.keyCode;
-  if (keyC === 37) goLeft();
-  else if (keyC === 38) goTop();
-  else if (keyC === 39) goRight();
-  else if (keyC === 40) goDown();
+  if (keyC === 37) addFun("left");
+  else if (keyC === 38) addFun("up");
+  else if (keyC === 39) addFun("right");
+  else if (keyC === 40) addFun("down");
   else if (keyC === 67) myBlock.style.backgroundColor = randomColor();
-  console.log(e.keyCode);
+  // console.log(e.keyCode);
 });
+
+function addFun(value) {
+  funList.push(value);
+  let span = document.createElement("span");
+  span.textContent = "+" + value;
+  span.style.padding = "10px";
+  span.style.border = "1px solid #ddd";
+  myFunctionList.appendChild(span);
+  span.addEventListener("mouseover", function () {
+    this.style.backgroundColor = "red";
+    this.style.color = "white";
+  });
+  span.addEventListener("mouseout", function () {
+    this.style.backgroundColor = "white";
+    this.style.color = "black";
+  });
+
+  console.log(funList);
+}
 
 function randomColor() {
   return "#" + Math.random().toString(16).substr(-6);
@@ -42,7 +65,7 @@ function goRight() {
   myBlock.style.left = temp + "px";
 }
 
-function goTop() {
+function goUp() {
   let temp = myBlock.offsetTop;
   temp -= 50;
   myBlock.style.top = temp + "px";
