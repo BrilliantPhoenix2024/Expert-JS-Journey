fb1466f8c62514fd2ffe57719e5b1997e3df4096
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-04-01 23:12:51 +0330

    Remove Duplicates from Array

--- BuildingProjects-PracticeJS/Email Extractor Application/script.js
+++ BuildingProjects-PracticeJS/Email Extractor Application/script.js
@@ -7,9 +7,16 @@ btn.addEventListener("click", function () {
   let temp = rawTxt.value;
   let exp = /([A-Za-z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi;
   let emailData = temp.match(exp);
-  console.log(emailData);
-  counter.innerHTML = emailData.length + " Emails Found.";
-  finTxt.innerHTML = emailData;
+  //   console.log(emailData);
+  let holder = [];
+  for (let x = 0; x < emailData.length; x++) {
+    if (holder.indexOf(emailData[x]) == -1) {
+      holder.push(emailData[x]);
+    }
+  }
+  let tempHolder = holder.join("--->");
+  counter.innerHTML = holder.length + " Emails Found.";
+  finTxt.innerHTML = tempHolder;
 });
 
 // https://regexr.com/
