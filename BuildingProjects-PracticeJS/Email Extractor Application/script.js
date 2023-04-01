74ef5aaff36adcf3bcd0f0c85cdf9af218c867fd
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-04-01 23:01:00 +0330

    Output Emails Only!

--- BuildingProjects-PracticeJS/Email Extractor Application/script.js
+++ BuildingProjects-PracticeJS/Email Extractor Application/script.js
@@ -1,12 +1,15 @@
 const rawTxt = document.querySelector("textarea[name=txtarea]");
 const finTxt = document.querySelector("textarea[name=output]");
 const btn = document.querySelector("button");
+const counter = document.querySelector(".counter");
 
 btn.addEventListener("click", function () {
   let temp = rawTxt.value;
   let exp = /([A-Za-z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi;
   let emailData = temp.match(exp);
   console.log(emailData);
+  counter.innerHTML = emailData.length + " Emails Found.";
+  finTxt.innerHTML = emailData;
 });
 
 // https://regexr.com/
