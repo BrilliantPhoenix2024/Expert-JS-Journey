981e0e5b22b5c2dcd0a28bd69f12d9bbbe261445
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-04-01 22:31:56 +0330

    Start Email Extractor Generator Project

---
+++ BuildingProjects-PracticeJS/Email Extractor Application/script.js
@@ -0,0 +1,7 @@
+const rawTxt = document.querySelector("textarea[name=txtarea]");
+const finTxt = document.querySelector("textarea[name=output]");
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", function () {
+  console.log("clicked!");
+});
