86f5f81ee1174caa3b7ccd74a5b8a85b7287161e
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-04-02 23:27:22 +0330

    RequestAnimationFrame Method

--- BuildingProjects-PracticeJS/Car Criving Game/script.js
+++ BuildingProjects-PracticeJS/Car Criving Game/script.js
@@ -1,9 +1,9 @@
 const score = document.querySelector(".score");
 const startScreen = document.querySelector(".startScreen");
-const gameArea = document.querySelecto(".gameArea");
+const gameArea = document.querySelector(".gameArea");
 let palyer = {
-  speed: 5,
-  score: 0,
+  //   speed: 5,
+  //   score: 0,
 };
 let keys = {
   ArrowUp: false,
@@ -15,6 +15,13 @@ let keys = {
 startScreen.addEventListener("click", start);
 document.addEventListener("keydown", pressOn);
 document.addEventListener("keyup", pressOff);
+
+function playGame() {
+  console.log("inplay");
+  if (palyer.start) {
+    window.requestAnimationFrame(playGame);
+  }
+}
 
 function pressOn(e) {
   e.preventDefault();
@@ -29,5 +36,12 @@ function pressOff(e) {
 }
 
 function start() {
-  console.log("click");
+  //   console.log("click");
+  startScreen.classList.remove("hide");
+  gameArea.classList.add("hide");
+  palyer.start = true;
+  window.requestAnimationFrame(playGame);
+  let car = document.createElement("div");
+  car.innerText = "Car";
+  gameArea.appendChild(car);
 }
