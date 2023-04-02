92f23a49a5f3c295b1b0907ac480664ebe710368
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-04-02 22:23:25 +0330

    Start Car Criving Game Project

---
+++ BuildingProjects-PracticeJS/Car Criving Game/script.js
@@ -0,0 +1,7 @@
+const score = document.querySelector(".score");
+const startScreen = document.querySelector(".startScreen");
+const gameArea = document.querySelecto(".gameArea");
+let palyer = {
+  speed: 5,
+  score: 0,
+};
