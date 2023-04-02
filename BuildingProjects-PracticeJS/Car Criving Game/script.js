568b88182276437f445f6ab808281a4ab1f1c53e
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-04-02 22:40:43 +0330

    KeyPress Event Listeners

--- BuildingProjects-PracticeJS/Car Criving Game/script.js
+++ BuildingProjects-PracticeJS/Car Criving Game/script.js
@@ -5,9 +5,29 @@ let palyer = {
   speed: 5,
   score: 0,
 };
+let keys = {
+  ArrowUp: false,
+  ArrowDown: false,
+  ArrowRight: false,
+  ArrowLeft: false,
+};
 
 startScreen.addEventListener("click", start);
+document.addEventListener("keydown", pressOn);
+document.addEventListener("keyup", pressOff);
+
+function pressOn(e) {
+  e.preventDefault();
+  keys[e.key] = true;
+  console.log(keys);
+}
+
+function pressOff(e) {
+  e.preventDefault();
+  keys[e.key] = false;
+  console.log(keys);
+}
 
 function start() {
-    console.log('click');
+  console.log("click");
 }
