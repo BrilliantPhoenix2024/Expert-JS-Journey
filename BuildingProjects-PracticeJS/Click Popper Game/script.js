0def9b6b526e60b74e79c8a76ffb615765ea623b
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-04-09 22:01:45 +0330

    Setup Player Stats update HTML

--- BuildingProjects-PracticeJS/Click Popper Game/script.js
+++ BuildingProjects-PracticeJS/Click Popper Game/script.js
@@ -6,3 +6,15 @@ playArea.main = document.querySelector(".main");
 playArea.game = document.querySelector(".main");
 playArea.btns = Array.from(document.querySelectorAll(".btn"));
 playArea.page = Array.from(document.querySelectorAll(".page"));
+
+player.score = 0;
+player.items = 3;
+
+playArea.btns.forEach(function (item) {
+  console.log(item);
+  item.addEventListener("click", handleBtn);
+});
+
+function handleBtn() {
+  console.log("hello");
+}
