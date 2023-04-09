08cb3db49e486067d51f4a394799b30ca7f2c158
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-04-09 21:56:54 +0330

    Select Elements JavaScript DOM

---
+++ BuildingProjects-PracticeJS/Click Popper Game/script.js
@@ -0,0 +1,8 @@
+const playArea = {};
+const player = {};
+
+playArea.stats = document.querySelector(".stats");
+playArea.main = document.querySelector(".main");
+playArea.game = document.querySelector(".main");
+playArea.btns = Array.from(document.querySelectorAll(".btn"));
+playArea.page = Array.from(document.querySelectorAll(".page"));
