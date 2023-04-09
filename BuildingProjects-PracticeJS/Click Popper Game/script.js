9354ac3a1ab44d54f1c7961cff71af8219b08207
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-04-09 23:30:03 +0330

    CSS with Start Game

--- BuildingProjects-PracticeJS/Click Popper Game/script.js
+++ BuildingProjects-PracticeJS/Click Popper Game/script.js
@@ -1,22 +1,35 @@
 const playArea = {};
 const player = {};
+let gameObj;
 
 playArea.stats = document.querySelector(".stats");
 playArea.main = document.querySelector(".main");
-playArea.game = document.querySelector(".main");
+playArea.game = document.querySelector(".game");
 playArea.btns = Array.from(document.querySelectorAll(".btn"));
 playArea.page = Array.from(document.querySelectorAll(".page"));
-
-player.score = 0;
-player.items = 3;
+document.addEventListener("DOMContentLoaded", getData);
 
 playArea.btns.forEach(function (item) {
   console.log(item);
   item.addEventListener("click", handleBtn);
 });
 
+function getData() {
+  playArea.main.classList.add("visible");
+  fetch("https://api.myjson.com/bins/gungm")
+    .then(function (rep) {
+      return rep.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      gameObj = data.data;
+      console.log(gameObj);
+    });
+  //   console.log("DOM loaded!");
+}
+
 function handleBtn(e) {
-  console.log(e.target.classList.contains("newGame"));
+  //   console.log(e.target.classList.contains("newGame"));
   if (e.target.classList.contains("newGame")) {
     console.log("YES");
     startGame();
@@ -24,5 +37,9 @@ function handleBtn(e) {
 }
 
 function startGame() {
+  player.score = 0;
+  player.items = 3;
+  playArea.main.classList.remove("visible");
+  playArea.game.classList.add("visible");
   console.log("start");
 }
