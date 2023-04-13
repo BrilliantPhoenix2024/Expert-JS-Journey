940dff22ec479c8d2d7bf734d4997cfc6d371f4d
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-04-13 20:29:18 +0330

    JavaScript Math Random

--- BuildingProjects-PracticeJS/Ultimate Dice Game/script.js
+++ BuildingProjects-PracticeJS/Ultimate Dice Game/script.js
@@ -5,4 +5,11 @@ const output = document.querySelector("output");
 
 button.addEventListener("click", function () {
   console.log("works!");
+  let rolls = [roll(6), roll(6)];
+  console.log(rolls);
 });
+
+function roll(number) {
+  let randomNum = Math.floor(Math.random() * number) + 1;
+  return randomNum;
+}
