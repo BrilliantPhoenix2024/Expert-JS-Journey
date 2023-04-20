fbfd21d676113e00e54b47506349503cd664610e
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-04-20 13:56:22 +0330

    Add Event Listeners

--- BuildingProjects-PracticeJS/Flying Bird Game/script.js
+++ BuildingProjects-PracticeJS/Flying Bird Game/script.js
@@ -2,3 +2,22 @@ const score = document.querySelector(".score");
 const startScreen = document.querySelector(".startScreen");
 const gameArea = document.querySelector(".gameArea");
 const gameMessage = document.querySelector(".gameMessage");
+
+gameMessage.addEventListener("click", start);
+startScreen.addEventListener("click", start);
+document.addEventListener("keydown", pressOn);
+document.addEventListener("keyUp", pressOff);
+
+function start() {
+  console.log("start!");
+  gameMessage.classList.add("hide");
+  startScreen.classList.add("hide");
+}
+
+function pressOn() {
+  console.log("On!");
+}
+
+function pressOff() {
+  console.log("Off!");
+}
