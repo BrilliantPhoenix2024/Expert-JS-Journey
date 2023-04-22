9088c35b0d623524327e92f80dfba9fd229bb8bf
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-04-23 01:18:43 +0330

    Game End Conditions

--- BuildingProjects-PracticeJS/Flying Bird Game/script.js
+++ BuildingProjects-PracticeJS/Flying Bird Game/script.js
@@ -14,6 +14,8 @@ function start() {
   console.log("start!");
   player.speed = 2;
   player.score = 0;
+  player.inplay = true;
+  gameArea.innerHTML = "";
   gameMessage.classList.add("hide");
   startScreen.classList.add("hide");
   let bird = document.createElement("div");
@@ -31,41 +33,54 @@ function start() {
 
 function playGame() {
   console.log(player);
-  let bird = document.querySelector(".bird");
-  let wing = document.querySelector(".wing");
-  let move = false;
+  if (player.inplay) {
+    let bird = document.querySelector(".bird");
+    let wing = document.querySelector(".wing");
+    let move = false;
 
-  if (keys.ArrowLeft && player.x > 0) {
-    player.x -= player.speed;
-    move = true;
-  }
-  if (keys.ArrowRight && player.x < gameArea.offsetWidth - 50) {
-    player.x += player.speed;
-    move = true;
-  }
-  if ((keys.ArrowUp || keys.Space) && player.y > 0) {
-    player.y -= player.speed * 5;
-    move = true;
-  }
-  if (keys.ArrowDown && player.y < gameArea.offsetHight - 50) {
-    player.y += player.speed;
-    move = true;
-  }
-  if (move) {
-    wing.pos = wing.pos == 15 ? 25 : 15;
-    wing.style.top = wing.pos + "px";
-  }
-  player.y += player.speed * 2;
-  if (bird.offsetTop > gameArea.offsetHight) {
-    console.log("Game over!");
-  }
+    if (keys.ArrowLeft && player.x > 0) {
+      player.x -= player.speed;
+      move = true;
+    }
+    if (keys.ArrowRight && player.x < gameArea.offsetWidth - 50) {
+      player.x += player.speed;
+      move = true;
+    }
+    if ((keys.ArrowUp || keys.Space) && player.y > 0) {
+      player.y -= player.speed * 5;
+      move = true;
+    }
+    if (keys.ArrowDown && player.y < gameArea.offsetHeight - 50) {
+      player.y += player.speed;
+      move = true;
+    }
+    if (move) {
+      wing.pos = wing.pos == 15 ? 20 : 15;
+      wing.style.top = wing.pos + "px";
+    }
+    player.y += player.speed * 2;
+    if (player.y > gameArea.offsetHeight) {
+      console.log("Game over!");
+      playGameOver(bird);
+    }
 
-  bird.style.top = player.y + "px";
-  bird.style.left = player.x + "px";
+    bird.style.top = player.y + "px";
+    bird.style.left = player.x + "px";
 
-  window.requestAnimationFrame(playGame);
-  player.score++;
-  score.innerText = "Score: " + player.score;
+    window.requestAnimationFrame(playGame);
+    player.score++;
+    score.innerText = "Score: " + player.score;
+  }
+}
+
+function playGameOver(bird) {
+  player.inplay = false;
+  gameMessage.classList.remove("hide");
+  bird.setAttribute("style", "transform:rotate(180deg)");
+  gameMessage.innerHTML =
+    "Game Over!<br> Your Score " +
+    player.score +
+    "<br>Click here to start again.";
 }
 
 function pressOn(e) {
