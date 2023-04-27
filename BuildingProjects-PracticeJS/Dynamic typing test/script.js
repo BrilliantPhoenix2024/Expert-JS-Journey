74b08a8e5e17e2373d8eacf3411961ce085ee333
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-04-27 22:51:01 +0330

    Typing Test Mini Application Setup

---
+++ BuildingProjects-PracticeJS/Dynamic typing test/script.js
@@ -0,0 +1,9 @@
+const wording = [
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid atque eum itaque illum, suscipit, quibusdam debitis earum optio deleniti asperiores, unde maiores at est quos. Accusantium aut asperiores atque totam.",
+];
+const message = document.querySelector(".message");
+const playText = document.querySelector("textarea");
+const button = document.querySelector("button");
+button.addEventListener("click", function () {
+  console.log(this.innerText);
+});
