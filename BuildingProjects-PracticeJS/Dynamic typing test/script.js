49dd501b92617a0de3959acf0255f1e7b36bc1a7
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-04-30 11:35:03 +0330

    JavaScript Timer Date

--- BuildingProjects-PracticeJS/Dynamic typing test/script.js
+++ BuildingProjects-PracticeJS/Dynamic typing test/script.js
@@ -3,9 +3,11 @@ const wording = [
   "Aliquid atque eum itaque illum, suscipit, quibusdam debitis earum optio deleniti asperiores",
   "Runde maiores at est quos. Accusantium aut asperiores atque totam.",
 ];
+let startTime, endTime;
 const message = document.querySelector(".message");
 const playText = document.querySelector("textarea");
 const button = document.querySelector("button");
+
 button.addEventListener("click", function () {
   console.log(this.innerText);
   if (this.innerText == "Start") {
@@ -17,4 +19,10 @@ button.addEventListener("click", function () {
 function playGame() {
   let randomNum = Math.floor(Math.random() * wording.length);
   message.innerText = wording[randomNum];
+  let date = new Date();
+  startTime = date.getTime;
+  console.log(startTime);
+  button.innerText = "Done";
+  
+  console.log(randomNum);
 }
