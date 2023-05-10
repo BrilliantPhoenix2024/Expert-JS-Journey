303ae38194babee3d3c228a62b360ca65267a9d2
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-05-10 19:29:00 +0330

    Game Area Setup

---
+++ BuildingProjects-PracticeJS/Card War Game/script.js
@@ -0,0 +1,5 @@
+const message = document.querySelector(".message");
+const buttons = document.querySelectorAll("button");
+const gameplay = document.querySelector(".gameplay");
+const userPlay = document.querySelector(".userPlay");
+const res = document.querySelector(".res");
