0aaa36961799553d42940edf65089fd16baa21e8
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-05-11 19:43:13 +0330

    Start the Game

--- BuildingProjects-PracticeJS/Card War Game/script.js
+++ BuildingProjects-PracticeJS/Card War Game/script.js
@@ -3,3 +3,27 @@ const buttons = document.querySelectorAll("button");
 const gameplay = document.querySelector(".gameplay");
 const userPlay = document.querySelector(".userPlay");
 const res = document.querySelector(".res");
+
+buttons.forEach(function (item) {
+  console.log(item);
+  item.addEventListener("click", playGame);
+});
+
+function playGame(e) {
+  let temp = e.target.textContent;
+  console.log(temp);
+  if (temp == "Start") {
+    btnToggle();
+    startGame();
+  }
+}
+
+function btnToggle() {
+  buttons[0].classList.toggle("hide");
+  buttons[1].classList.toggle("hide");
+}
+
+function startGame() {
+  let numberPlayers = document.querySelector("input").value;
+  document.querySelector("input").value = "1";
+}
