225cb7c48b0b9ad9036d37c3b1205e5504410bff
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-05-13 20:13:19 +0330

    Create Deck of Cards

--- BuildingProjects-PracticeJS/Card War Game/script.js
+++ BuildingProjects-PracticeJS/Card War Game/script.js
@@ -4,6 +4,10 @@ const gameplay = document.querySelector(".gameplay");
 const userPlay = document.querySelector(".userPlay");
 const res = document.querySelector(".res");
 
+let deck = [];
+const ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+const suits = ["hearts", "dimas", "clubs", "spades"];
+
 buttons.forEach(function (item) {
   console.log(item);
   item.addEventListener("click", playGame);
@@ -25,5 +29,20 @@ function btnToggle() {
 
 function startGame() {
   let numberPlayers = document.querySelector("input").value;
+  buildDeck();
+  console.log(deck);
   document.querySelector("input").value = "1";
+}
+
+function buildDeck() {
+  deck = [];
+  for (let i = 0; i < suits.length; i++) {
+    for (let j = 0; j < ranks.length; j++) {
+      let card = {};
+      card.suit = suits[i];
+      card.rank = ranks[j];
+      card.value = j + 1;
+      deck.push(card);
+    }
+  }
 }
